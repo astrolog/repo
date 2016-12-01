7721dd32dce1c228538bbdc134ef0af4598bd9fe
astrolog <jtymowski@gmail.com>
2016-12-01 20:20:06 +0100

    ch

--- publish.bat
+++
@@ -1,5 +0,0 @@
-C:\Users\qba\AppData\Local\GitHub\PortableGit_d7effa1a4a322478cd29c826b52a0c118ad3db11\cmd\git.exe add .
-C:\Users\qba\AppData\Local\GitHub\PortableGit_d7effa1a4a322478cd29c826b52a0c118ad3db11\cmd\git.exe commit -m "ch"
-C:\Users\qba\AppData\Local\GitHub\PortableGit_d7effa1a4a322478cd29c826b52a0c118ad3db11\cmd\git.exe push origin gh-pages
-
-timeout 10
